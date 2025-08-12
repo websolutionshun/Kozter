876c517860c52059744026e48a3e478a2d2330ca
Peter Sipos <server@websolutions.hu>
2025-08-12 16:53:16 +0200

    A navigációs menü frissítése: a "Home" linket a frontend URL-re módosítottuk, hogy új ablakban nyíljon meg. Ez javítja a felhasználói élményt és a navigációs struktúrát.

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -318,7 +318,7 @@ AppAsset::register($this);
                         <!-- BEGIN NAVBAR MENU -->
                         <ul class="navbar-nav">
                             <li class="nav-item">
-                                <a class="nav-link" href="./">
+                                <a class="nav-link" href="<?php echo env('FRONTEND_URL'); ?>" target="_blank">
                                     <span
                                         class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler.io/icons/icon/home -->
                                         <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-eye"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" /><path d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6" /></svg></span>
