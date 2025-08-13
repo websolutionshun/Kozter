63fc61e8d65d253a8d6671ccd5d1a18e3fb4af9f
Peter Sipos <server@websolutions.hu>
2025-08-13 16:47:06 +0200

    Felhasználói felület frissítése: a jogosultságok és felhasználók kezelése gombok stílusának módosítása, a gombok méretének csökkentése érdekében. A jogosultságok táblázatában a felesleges gombok eltávolítása a felhasználói élmény javítása érdekében.

--- backend/views/permission/index.php
+++ backend/views/permission/index.php
@@ -89,23 +89,6 @@ $this->params['breadcrumbs'][] = $this->title;
                             </svg>
                             Módosítások automatikusan mentődnek
                         </div>
-                        <div class="btn-list">
-                            <?= Html::a('
-                                <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                                    <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0"/>
-                                    <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2"/>
-                                </svg>
-                                Szerepkörök kezelése', ['/role/index'], ['class' => 'btn btn-primary']) ?>
-                            <?= Html::a('
-                                <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                                    <circle cx="12" cy="12" r="3"/>
-                                    <path d="M12 1v6m0 6v6"/>
-                                    <path d="M21 12h-6m-6 0H3"/>
-                                </svg>
-                                Felhasználók kezelése', ['/user/index'], ['class' => 'btn btn-secondary']) ?>
-                        </div>
                     </div>
                 </div>
             </div>
